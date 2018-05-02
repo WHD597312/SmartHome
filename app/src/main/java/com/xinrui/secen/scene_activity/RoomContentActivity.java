a8fe8c945b1038a5db036efbabda31033c46fe6d
jiangjieyu <jiananqi@foxmail.com>
2018-05-02 19:14:28 +0800

    提交

--- app/src/main/java/com/xinrui/secen/scene_activity/RoomContentActivity.java
+++ app/src/main/java/com/xinrui/secen/scene_activity/RoomContentActivity.java
@@ -727,6 +727,7 @@ public class RoomContentActivity extends Activity {
 //                    getData(et,eh);
 //                    break;
                     if(deviceChild2!=null){
+                        
                         String macAddress2 = deviceChild2.getMacAddress();
                         if(macAddress2.equals(macAddress)){
                             extTemp1.setText(deviceChild2.getTemp()+"℃");
